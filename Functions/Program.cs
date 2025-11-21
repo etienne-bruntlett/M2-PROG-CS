800739465827501f086f4a3aa75ba11ab2002910
Etienne <40747@ma-web.nl>
2025-11-21 12:23:07 +0100

    commit3

--- Functions/Program.cs
+++ Functions/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace Functions;
-
-class Program
+﻿namespace Functions
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
+    internal class Program{
+        static void Main(string[] args)
+        {
+            Program program = new Program(); // er wordt een nieuwe variabele gemaakt: 'program'. dit variabel wordt gebruikt om 'Run' op een specifieke manier uit te voeren
+            program.Run(); // de 'Run' functie wordt opgevraagd. de return type is void
+        }
+
+        void Run(){
+            Console.WriteLine("Dit is de start van mijn programma.");
+        }
     }
 }
