dc0b5d5f3564ec9b4c92ec60765256fe957bcdbc
Etienne <40747@ma-web.nl>
2025-11-21 12:43:17 +0100

    commit4

--- Functions/Program.cs
+++ Functions/Program.cs
@@ -7,8 +7,62 @@
             program.Run(); // de 'Run' functie wordt opgevraagd. de return type is void
         }
 
-        void Run(){
-            Console.WriteLine("Dit is de start van mijn programma.");
+        internal void Run(){
+            Console.WriteLine("...");
+            Vraag1();
+            Vraag2();
+            Vraag3();
+            Vraag4();
+            Vraag5();
+            Vraag6();
+        }
+
+        internal void Vraag1()
+        {
+            Console.WriteLine("Pancakes or waffles?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+
+        internal void Vraag2()
+        {
+            Console.WriteLine("What is something you can never seem to finish?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+
+        internal void Vraag3()
+        {
+            Console.WriteLine("What's the last book you gave up on and stopped reading?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+
+        internal void Vraag4()
+        {
+            Console.WriteLine("In your group of friends, what role do you play?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+
+        internal void Vraag5()
+        {
+            Console.WriteLine("What's your favorite piece of clothing you own?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+
+        internal void Vraag6()
+        {
+            Console.WriteLine("What have you created that you are most proud of?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
         }
     }
 }
